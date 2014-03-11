a01440608ce4725075ce84dd33101e5af4cfac4f
Baptiste Grenier <bgrenier@gnubila.fr>
2014-03-11 13:45:02 +0100

    Install oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 mkdir -p "$HOME/repos/gwarf"
 cd $_
