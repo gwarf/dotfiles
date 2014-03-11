0c38537e955c2fe91b56b2feec7dcf3552bc0b01
Baptiste Grenier <bgrenier@gnubila.fr>
2014-03-11 13:46:55 +0100

    Install oh-my-zsh. Log steps. Add shebang. #lazy committer.

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
+#!/bin/sh
+
+echo 'Installing Oh-my-zsh.'
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+echo 'Installing prezto.'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+echo 'Cloning dotfiles'
 mkdir -p "$HOME/repos/gwarf"
 cd $_
 git clone git://github.com/gwarf/dotfiles.git
+
+echo 'Symlink-ing dotfiles'
 cd dotfiles
 ./symlink-dotfiles.sh
