1cedd192053ab98c6b17f2e8c0bf89c793dd55c2
Baptiste Grenier <bgrenier@gnubila.fr>
2014-03-11 14:36:54 +0100

    Install vim conf.

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 echo 'Installing prezto.'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
+echo 'Installing vim conf.'
+curl -L https://raw.github.com/gwarf/vim/master/install.sh | sh
+
 echo 'Cloning dotfiles'
 mkdir -p "$HOME/repos/gwarf"
 cd $_
