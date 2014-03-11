e5eff468c74f47c43054bce2bc6b6f8ba9b1aafb
Baptiste Grenier <bgrenier@gnubila.fr>
2014-03-11 13:26:32 +0100

    Add a script for symlinking configuration.

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+dotfiles="~/repos/gwarf/dotfiles"
+
+if [ -d "$dotfiles" ]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+link_conf() {
+  from="$1"
+  to="$2"
+  echo "Linking configuration '$from' to '$to'"
+  rm -f "$to"
+  ln -s "$from" "$to"
+}
+
+for configfile in $(ls $dotfiles/.config); do
+  link_conf "$dotfiles/.config/$configfile" "$HOME/.config/$configfile"
+done
