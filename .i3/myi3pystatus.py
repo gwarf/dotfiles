843aa9ddc117e9d16e2919b3ecabd4e7510daff4
Baptiste Grenier <bgrenier@gnubila.fr>
2014-03-27 15:05:11 +0100

    i3: disable useless info.

--- .i3/myi3pystatus.py
+++ .i3/myi3pystatus.py
@@ -17,8 +17,8 @@ status.register("clock",
 status.register("load")
 
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
+#status.register("temp",
+#    format="{temp:.0f}°C",)
 
 # The battery monitor has many formatting options, see README for details
 
@@ -78,13 +78,13 @@ status.register("temp",
 # Shows disk usage of /
 # Format:
 # 42/128G [86G]
-status.register("disk",
-    path="/",
-    format="/: {used}/{total}G [{avail}G]",)
-
-status.register("disk",
-    path="/home",
-    format="/home: {used}/{total}G [{avail}G]",)
+#status.register("disk",
+#    path="/",
+#    format="/: {used}/{total}G [{avail}G]",)
+#
+#status.register("disk",
+#    path="/home",
+#    format="/home: {used}/{total}G [{avail}G]",)
 
 # Shows pulseaudio default sink volume
 #
