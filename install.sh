2bcc07037092796819aabbba04d68dd3831b8e80
Baptiste Grenier <bgrenier@gnubila.fr>
2014-09-02 18:41:40 +0200

    install.sh: comment oh-my-zsh installation.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo 'Installing Oh-my-zsh.'
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#echo 'Installing Oh-my-zsh.'
+#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 echo 'Installing prezto.'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
