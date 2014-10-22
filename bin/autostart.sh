a6592479e63e188774427714664e88e7b68a3f80
Baptiste Grenier <bgrenier@gnubila.fr>
2014-10-22 10:54:09 +0200

    autostart: remove commented xmodmap code.

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -30,9 +30,6 @@ xbacklight -set 90
 setxkbmap us -option 'compose:ralt'
 
 ## Load Xmodmap conf
-#xmodmap -e "remove Lock = Caps_Lock"
-#xmodmap -e "keysym Caps_Lock = Control_L"
-#xmodmap -e "add Control = Control_L"
 xmodmap ~/.Xmodmap
 
 ## Load Xresources
