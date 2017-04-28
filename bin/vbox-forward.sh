cae0ae06b8ddedbdbd1ad998657924ed0f42ae93
Baptiste Grenier <baptiste@bapt.name>
2017-04-28 09:48:24 +0200

    vbox-forward: fix interface name

--- bin/vbox-forward.sh
+++ bin/vbox-forward.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Enable forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -q -w net.ipv4.ip_forward=1
 
 # Activate Masquerading of host-only network
-sudo iptables -A FORWARD -o eth0 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -o enp0s31f6 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE
