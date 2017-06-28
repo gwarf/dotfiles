ee03b02039cc77cb81707292f251058a1cf390f7
Baptiste Grenier <baptiste@bapt.name>
2017-06-28 15:54:05 +0200

    vbox-forward: allow to configure output interface

--- bin/vbox-forward.sh
+++ bin/vbox-forward.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+OUT_IF=enp0s31f6
+#OUT_IF=wlp4s0
+
 # Enable forwarding
 sudo sysctl -q -w net.ipv4.ip_forward=1
 
 # Activate Masquerading of host-only network
-sudo iptables -A FORWARD -o enp0s31f6 -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -o $OUT_IF -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE
