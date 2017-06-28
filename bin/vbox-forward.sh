c6a97e3af353d2de98419600c3736da366c2deee
Baptiste Grenier <baptiste@bapt.name>
2017-06-28 16:17:29 +0200

    vbox-forward: dynamically find default output interface

--- bin/vbox-forward.sh
+++ bin/vbox-forward.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-OUT_IF=enp0s31f6
-#OUT_IF=wlp4s0
+# Enable masquerading of host-only virtualbox network
+
+VBOX_NETWORK='192.168.56.0/24'
+
+OUT_IF=$(ip route show to 0.0.0.0/0 | awk '{print $5}')
+LOCAL_IFS=$(ip link show up | grep -B1 'link/ether' | awk '/^[0-9]*:/ {print $2}' | sed 's/:$//')
+
+if ! echo "$LOCAL_IFS" | grep -q "$OUT_IF"; then
+  printf "$OUT_IF is invalid!\n"
+  printf "Output interface should be one of:\n"
+  printf "$LOCAL_IFS\n"
+  exit 1
+fi
 
 # Enable forwarding
 sudo sysctl -q -w net.ipv4.ip_forward=1
 
 # Activate Masquerading of host-only network
-sudo iptables -A FORWARD -o $OUT_IF -i vboxnet0 -s 192.168.56.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -o "$OUT_IF" -i vboxnet0 -s "$VBOX_NETWORK" -m conntrack --ctstate NEW -j ACCEPT
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE
