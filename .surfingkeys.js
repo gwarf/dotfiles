27c53ebe6de3fe57e6e8e6c6e8da87e20e251ad5
Baptiste Grenier <baptiste@bapt.name>
2017-09-13 16:06:56 +0200

    Import Surkfing Keys conf for chrome

---
+++ .surfingkeys.js
@@ -0,0 +1,83 @@
+// https://brookhong.github.io/2015/09/15/surfingkeys.html
+
+// align then hint to the right
+settings.hintAlign = "left";
+
+// hint characters
+Hints.characters = "asdfghjkl;";
+
+// intercept all error pages so Surfing keys can work on them
+settings.interceptedErrors = ["*"];
+
+// turn off smoothscroll
+settings.smoothScroll = false;
+
+// show the current mode in the status bar
+settings.showModeStatus = true;
+
+// open new tabs to the right of the current one
+settings.newTabPosition = "right";
+
+// disable smart page boundry jump next/previous
+settings.smartPageBoundary = false;
+
+// make Google translate languages clickable
+settings.clickableSelector = "*.jfk-button, *.goog-flat-menu-button";
+
+// show only tabs from the current window
+settings.omnibarTabsQuery = {currentWindow: true};
+
+// Follow link?
+// map f
+
+// Follow in new active tab: af
+//mapkey ('F', 'Open link in new tab', 'Hints.create (Hints.pointers, Hints.dispatchMouseClick, {tabbed: true})');
+
+// Follow in new background tab: gf
+// map gf
+// mapkey ('gf', 'open a new tab in the background link', 'Hints.create (Hints.pointers, Hints.dispatchMouseClick, {tabbed: true, active: false})');
+
+// Previous page in history: S
+// mapkey ('H', 'backward', 'history.go (-1)');
+
+// Change scroll target (iframe): cs
+
+// Search with google (selected word or clipboard)
+// Launch search: sg
+// Launch search adding site:XXX: sog
+// Edit search: sG
+// Edit search adding site:XXX: soG
+// g is the searchalias for google define in the chrome conf
+addSearchAliasX('d', 'duckduckgo', 'https://duckduckgo.com/?q=', 'o');
+addSearchAliasX('n', 'nar', 'http://newalbumreleases.net/?s=', 'o');
+addSearchAliasX('q', 'qwant', 'https://www.qwant.com/?q=', 'o');
+addSearchAliasX('r', 'drive', 'https://drive.google.com/drive/search?q=', 'o');
+addSearchAliasX('gh', 'github', 'https://github.com/search?q=', 'o');
+addSearchAliasX('wke', 'wikipedia_en', 'https://en.wikipedia.org/w/index.php?search=', 'o');
+addSearchAliasX('wkf', 'wikipedia_fr', 'https://fr.wikipedia.org/w/index.php?search=', 'o');
+settings.defaultSearchEngine = 'd';
+
+// Edit URL to open in new tab
+// su
+
+// Edit settings: se
+
+// Edit input with vim editor: I
+
+// Markdown preview from clipboard
+// sm (preview markdown)
+// sm (show/edit markdown)
+// :Wq (update preview)
+
+// Show usage: ?: us
+// Search with default search engine or open from clipboard
+// map p in new tab
+// map P in current tab
+
+// Add bookmark with a
+
+// Access tab/buffer with b
+
+// Disable for sites X
+
+// vim:ft=javascript:foldmethod=marker:foldlevel=0:
