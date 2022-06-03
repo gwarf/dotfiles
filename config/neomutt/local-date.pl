ea85ecc73d63bc4ce9e47a135429bd1b6d7e1204
Baptiste Grenier <baptiste@bapt.name>
2022-06-03 10:55:39 +0200

    Import local-date

---
+++ config/neomutt/local-date.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl -n
+
+use Date::Parse;
+use POSIX;
+use strict;
+
+if (/^Date: (.*)$/) {
+    my $datestr = $1;
+    my $date = strftime ("%a, %d %b %Y %H:%M:%S",
+                         localtime (str2time ($datestr)));
+    print "Local-Date: $date\n";
+}
+
+print;
