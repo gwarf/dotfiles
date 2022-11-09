d2aeb5f7425fe06296db3189f453c1066bd22135
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-09 16:17:14 +0100

    launch_iterm.sh: hardcode nvim

--- .config/neomutt/launch_iterm.sh
+++ .config/neomutt/launch_iterm.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 osascript - "$@" <<EOF
-on run argv
-tell application "iTerm"
+on run input
+  set cmd to "nvim " & quote & input & quote & " && exit"
+  tell application "iTerm"
     activate
     set new_term to (create window with default profile)
     tell new_term
-        tell the current session
-            repeat with arg in argv
-               write text arg
-            end repeat
-        end tell
+      tell the current session
+         write text cmd
+      end tell
     end tell
-end tell
+  end tell
 end run
 EOF
