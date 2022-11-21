5ffb4e79fd8ef2df0832004df93c4c429febdad1
Baptiste Grenier <baptiste@bapt.name>
2022-11-21 22:10:40 +0100

    nvim: no more run packsersync automatically

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,12 +16,13 @@ end
 local packer_bootstrap = ensure_packer()
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- XXX disabled, a bit too annoying :)
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
