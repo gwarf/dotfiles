d813191bbdb7cc028be4e2e829be544a51ddddfc
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-22 17:50:56 +0100

    LSP: add pyright and yamlls

--- .config/nvim/lua/config/mason-lspconfig.lua
+++ .config/nvim/lua/config/mason-lspconfig.lua
@@ -10,11 +10,14 @@ require("mason-lspconfig").setup({
     "ltex",
     "marksman",
     --  "prosemd_lsp",
+    --  Struggling with plugins and pylint
+    --  https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/server_configurations/pylsp/README.md
     "pylsp",
+    "pyright",
     --  "ruby_ls",
     "sumneko_lua",
     "vimls",
-    -- "yamlls",
+    "yamlls",
   },
   automatic_installation = true,
 })
