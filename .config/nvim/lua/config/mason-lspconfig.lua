13f93de36957ee32129b2edd7193c533155c597d
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-23 16:23:08 +0100

    Merge content for mason-tool-installer.

--- .config/nvim/lua/config/mason-lspconfig.lua
+++ .config/nvim/lua/config/mason-lspconfig.lua
@@ -1,4 +1,23 @@
 require("mason").setup()
+require("mason-tool-installer").setup({
+  ensure_installed = {
+    "alex",
+    "actionlint",
+    "black",
+    "beautysh",
+    "isort",
+    -- "jedi-language-server",
+    "stylua",
+    "markdownlint",
+    "pylint",
+    "prettier",
+    "shellcheck",
+    "shfmt",
+  },
+  auto_update = true,
+  run_on_start = true,
+  tart_delay = 3000, -- 3 second delay
+})
 require("mason-lspconfig").setup({
   ensure_installed = {
     "ansiblels",
