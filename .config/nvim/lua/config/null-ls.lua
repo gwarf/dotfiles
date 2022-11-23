21a90212aa27b40e64e07b955fae4a05192fd384
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-23 15:04:02 +0100

    null-ls: disable auto format, enable ansiblelint, isort and flake8

--- .config/nvim/lua/config/null-ls.lua
+++ .config/nvim/lua/config/null-ls.lua
@@ -13,16 +13,18 @@ null_ls.setup({
     null_ls.builtins.code_actions.shellcheck,
     -- formatters
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.stylua,
     -- diagnostics
+    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.alex,
     null_ls.builtins.diagnostics.markdownlint.with({
       extra_args = { "--config", vim.fn.expand("~/.config/nvim/.markdownlint.json") },
     }),
     null_ls.builtins.diagnostics.shellcheck,
-    -- null_ls.builtins.diagnostics.ansiblelint,
+    null_ls.builtins.diagnostics.ansiblelint,
     -- null_ls.builtins.diagnostics.checkmake,
     -- null_ls.builtins.diagnostics.write_good,
     -- completion
@@ -31,19 +33,20 @@ null_ls.setup({
     -- hover
     null_ls.builtins.hover.dictionary, -- dictionary
   },
+  diagnostics_format = "[#{c}] #{m} (#{s})",
   -- format file on save
   -- you can reuse a shared lspconfig on_attach callback here
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+  --         vim.lsp.buf.format({ bufnr = bufnr })
+  --       end,
+  --     })
+  --   end
+  -- end,
 })
