a47ee0e667f42ab97349ea7c3cffd40b65524914
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-23 16:30:28 +0100

    lsp: import content from null-ls

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -7,6 +7,7 @@ local keymap = vim.keymap
 local lsp = vim.lsp
 local lspconfig = require("lspconfig")
 local utils = require("utils")
+local null_ls = require("null-ls")
 
 -- global config for diagnostic
 diagnostic.config({
@@ -297,3 +298,58 @@ if utils.executable("clangd") then
 else
   vim.notify("clangd not found!", vim.log.levels.WARN, { title = "Nvim-config" })
 end
+
+-- https://smarttech101.com/nvim-lsp-set-up-null-ls-for-beginners/
+-- https://github.com/Clumsy-Coder/dotfiles/commit/e81edc159f3fc9ef189e0300d280461e75732a4b
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- Available builtins: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+-- Check if packate is available using :echo executable("shfmt")
+
+null_ls.setup({
+  sources = {
+    -- code actions
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.code_actions.shellcheck,
+    -- formatters
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.beautysh,
+    null_ls.builtins.formatting.stylua,
+    -- diagnostics
+    null_ls.builtins.diagnostics.actionlint,
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.alex,
+    null_ls.builtins.diagnostics.markdownlint.with({
+      extra_args = { "--config", vim.fn.expand("~/.config/nvim/.markdownlint.json") },
+    }),
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.ansiblelint,
+    null_ls.builtins.diagnostics.zsh,
+    -- null_ls.builtins.diagnostics.checkmake,
+    -- null_ls.builtins.diagnostics.write_good,
+    -- completion
+    -- XXX spell is disabled
+    -- null_ls.builtins.completion.spell,
+    -- hover
+    null_ls.builtins.hover.dictionary, -- dictionary
+  },
+  diagnostics_format = "[#{c}] #{m} (#{s})",
+  -- format file on save
+  -- you can reuse a shared lspconfig on_attach callback here
+  -- on_attach = function(client, bufnr)
+  --   if client.supports_method("textDocument/formatting") then
+  --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --     vim.api.nvim_create_autocmd("BufWritePre", {
+  --       group = augroup,
+  --       buffer = bufnr,
+  --       callback = function()
+  --         -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+  --         vim.lsp.buf.format({ bufnr = bufnr })
+  --       end,
+  --     })
+  --   end
+  -- end,
+})
