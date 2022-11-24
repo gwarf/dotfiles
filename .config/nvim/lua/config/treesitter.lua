80209e440f192ef9b78bad8b0da74302ebfd11fd
Baptiste Grenier <baptiste@bapt.name>
2022-11-24 22:49:29 +0100

    Install treesitter parsers async.

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -39,7 +39,7 @@ ts_configs.setup({
     "yaml",
   },
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
+  sync_install = false,
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
