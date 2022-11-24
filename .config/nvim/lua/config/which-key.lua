95028322427818fb2ebf55b89085e1f17172f2c7
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-11-24 18:49:37 +0100

    Cleaning, reorganisation

--- .config/nvim/lua/config/which-key.lua
+++ .config/nvim/lua/config/which-key.lua
@@ -1,23 +1,43 @@
+-- see https://neovim.io/doc/user/map.html
 local which_key = require("which-key")
 
 which_key.setup({
   window = {
     border = "single", -- none, single, double, shadow
+    -- position = "bottom", -- bottom, top
+    -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    -- padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    -- winblend = 0,
+  },
+  -- layout = {
+  --   height = { min = 4, max = 25 }, -- min and max height of the columns
+  --   width = { min = 20, max = 50 }, -- min and max width of the columns
+  --   spacing = 3, -- spacing between columns
+  --   align = "left", -- align columns left, center or right
+  -- },
+  plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
+  },
+  -- popup_mappings = {
+  --   scroll_down = "<c-d>", -- binding to scroll down inside the popup
+  --   scroll_up = "<c-u>", -- binding to scroll up inside the popup
+  -- },
+  -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+  show_help = true, -- show help message on the command line when the popup is visible
+  triggers = "auto", -- automatically setup triggers
+  -- triggers = {"<leader>"} -- or specify a list manually
+  triggers_blacklist = {
+    -- list of mode / prefixes that should never be hooked by WhichKey
+    -- this is mostly relevant for key maps that start with a native binding
+    -- most people should not need to change this
+    i = { "j", "k" },
+    v = { "j", "k" },
   },
 })
 
--- wk.register({
---   f = {
---     name = "file", -- optional group name
---     f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
---     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false, buffer = 123 }, -- additional options for creating the keymap
---     n = { "New File" }, -- just a label. don't create any mapping
---     e = "Edit File", -- same as above
---     ["1"] = "which_key_ignore",  -- special label to hide it in the popup
---     b = { function() print("bar") end, "Foobar" }, -- you can also pass functions!
---     ["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle linewise (visual)" },
---   },
--- }, { prefix = "<leader>" })
 
 -- Mapping for normal mode
 local opts = {
@@ -85,77 +105,7 @@ local mappings = {
     q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
     U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
   },
-  l = {
-    name = "Logs",
-    d = {
-      "<cmd>lua require('lvim.core.terminal').toggle_log_view(require('lvim.core.log').get_path())<cr>",
-      "view default log",
-    },
-    D = {
-      "<cmd>lua vim.fn.execute('edit ' .. require('lvim.core.log').get_path())<cr>",
-      "Open the default logfile",
-    },
-    l = {
-      "<cmd>lua require('lvim.core.terminal').toggle_log_view(vim.lsp.get_log_path())<cr>",
-      "view lsp log",
-    },
-    L = { "<cmd>lua vim.fn.execute('edit ' .. vim.lsp.get_log_path())<cr>", "Open the LSP logfile" },
-    n = {
-      "<cmd>lua require('lvim.core.terminal').toggle_log_view(os.getenv('NVIM_LOG_FILE'))<cr>",
-      "view neovim log",
-    },
-    N = { "<cmd>edit $NVIM_LOG_FILE<cr>", "Open the Neovim logfile" },
-    p = {
-      "<cmd>lua require('lvim.core.terminal').toggle_log_view(get_cache_dir() .. '/packer.nvim.log')<cr>",
-      "view packer log",
-    },
-    P = { "<cmd>edit $LUNARVIM_CACHE_DIR/packer.nvim.log<cr>", "Open the Packer logfile" },
-  },
-  s = {
-    name = "Search",
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    f = { "<cmd>Telescope find_files<cr>", "Find File" },
-    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    M = { "<cmd>Telescope projects<cr>", "Projects" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-    R = { "<cmd>Telescope registers<cr>", "Registers" },
-    t = { "<cmd>Telescope live_grep<cr>", "Text" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-    p = {
-      "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
-      "Colorscheme with Preview",
-      y = { "<cmd>Telescope yank_history<cr>", "Yank history" },
-    },
-  },
-  -- Trouble
-  t = {
-    name = "Trouble",
-    d = { ":TroubleToggle document_diagnostics<cr>", "Toogle document diagnostics" },
-    l = { ":TroubleToggle loclist<cr>", "Toogle loclist" },
-    q = { ":TroubleToggle quickfix<cr>", "Toogle quickfix" },
-    t = { ":TroubleToggle lsp_references<cr>", "Toogle LSP references" },
-    t = { ":TroubleToggle<cr>", "Toogle window" },
-    w = { ":TroubleToggle workspace_diagnostics<cr>", "Toogle workspace diagnostics" },
-  },
-  -- XXX looks like duplicated
-  T = {
-    name = "Treesitter",
-    i = { ":TSConfigInfo<cr>", "Info" },
-  },
-  p = {
-    name = "Packer",
-    c = { "<cmd>PackerCompile<cr>", "Compile" },
-    i = { "<cmd>PackerInstall<cr>", "Install" },
-    -- r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
-    s = { "<cmd>PackerSync<cr>", "Sync" },
-    S = { "<cmd>PackerStatus<cr>", "Status" },
-    u = { "<cmd>PackerUpdate<cr>", "Update" },
-  },
-  --
+  -- Git
   g = {
     name = "Git",
     --   g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
@@ -183,25 +133,23 @@ local mappings = {
       "Git Diff",
     },
   },
+  -- LSP
   l = {
     name = "LSP",
+    -- map("n", "<space>e", vim.diagnostic.open_float, { desc = "Open diagnostic"})
+    -- map("n", "<space>wa", lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
+    -- map("n", "<space>wl", function() vim.inspect(lsp.buf.list_workspace_folders()) end, { desc = "list workspace folder" })
+    -- map("n", "<space>wr", lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
     d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
-    w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
-    -- Format file
+    e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
     f = { "<cmd>lua vim.lsp.buf.format(nil, 10000)<cr>", "Format" },
-    -- Format selection
     F = { "<cmd>lua vim.lsp.buf.range_formating()<cr>", "Format selection" },
+    h = { vim.lsp.buf.signature_help, "Signature help" },
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>Mason<cr>", "Mason Info" },
-    j = {
-      vim.diagnostic.goto_next,
-      "Next Diagnostic",
-    },
-    k = {
-      vim.diagnostic.goto_prev,
-      "Prev Diagnostic",
-    },
+    j = { vim.diagnostic.goto_next, "Next Diagnostic", },
+    k = { vim.diagnostic.goto_prev, "Prev Diagnostic", },
     l = { vim.lsp.codelens.run, "CodeLens Action" },
     q = { vim.diagnostic.setloclist, "Quickfix" },
     r = { vim.lsp.buf.rename, "Rename" },
@@ -209,8 +157,81 @@ local mappings = {
     S = {
       "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
       "Workspace Symbols",
+    w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
     },
-    e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+  },
+  -- Logs
+  L = {
+    name = "Logs",
+    d = {
+      "<cmd>lua require('lvim.core.terminal').toggle_log_view(require('lvim.core.log').get_path())<cr>",
+      "view default log",
+    },
+    D = {
+      "<cmd>lua vim.fn.execute('edit ' .. require('lvim.core.log').get_path())<cr>",
+      "Open the default logfile",
+    },
+    l = {
+      "<cmd>lua require('lvim.core.terminal').toggle_log_view(vim.lsp.get_log_path())<cr>",
+      "view lsp log",
+    },
+    L = { "<cmd>lua vim.fn.execute('edit ' .. vim.lsp.get_log_path())<cr>", "Open the LSP logfile" },
+    n = {
+      "<cmd>lua require('lvim.core.terminal').toggle_log_view(os.getenv('NVIM_LOG_FILE'))<cr>",
+      "view neovim log",
+    },
+    N = { "<cmd>edit $NVIM_LOG_FILE<cr>", "Open the Neovim logfile" },
+    p = {
+      "<cmd>lua require('lvim.core.terminal').toggle_log_view(get_cache_dir() .. '/packer.nvim.log')<cr>",
+      "view packer log",
+    },
+    P = { "<cmd>edit $LUNARVIM_CACHE_DIR/packer.nvim.log<cr>", "Open the Packer logfile" },
+  },
+  -- Packer
+  p = {
+    name = "Packer",
+    c = { "<cmd>PackerCompile<cr>", "Compile" },
+    i = { "<cmd>PackerInstall<cr>", "Install" },
+    -- r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
+    s = { "<cmd>PackerSync<cr>", "Sync" },
+    S = { "<cmd>PackerStatus<cr>", "Status" },
+    u = { "<cmd>PackerUpdate<cr>", "Update" },
+  },
+  -- Search with Telescope
+  s = {
+    name = "Search",
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+    f = { "<cmd>Telescope find_files<cr>", "Find File" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    p = { "<cmd>Telescope projects<cr>", "Projects" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+    R = { "<cmd>Telescope registers<cr>", "Registers" },
+    t = { "<cmd>Telescope live_grep<cr>", "Text" },
+    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+    C = { "<cmd>Telescope commands<cr>", "Commands" },
+    P = {
+      "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+      "Colorscheme with Preview",
+    },
+    y = { "<cmd>Telescope yank_history<cr>", "Yank history" },
+  },
+  -- Trouble
+  t = {
+    name = "Trouble",
+    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document diagnostics" },
+    l = { "<cmd>TroubleToggle loclist<cr>", "Toogle loclist" },
+    o = { "<cmd>TroubleToggle<cr>", "Toogle window" },
+    q = { "<cmd>TroubleToggle quickfix<cr>", "Toogle quickfix" },
+    t = { "<cmd>TroubleToggle lsp_references<cr>", "Toogle LSP references" },
+    w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace diagnostics" },
+  },
+  -- Treesitter
+  T = {
+    name = "Treesitter",
+    i = { "<cmd>TSConfigInfo<cr>", "Info" },
   },
 }
 
@@ -223,81 +244,9 @@ local vopts = {
   noremap = true, -- use `noremap` when creating keymaps
   nowait = true, -- use `nowait` when creating keymaps
 }
--- NOTE: Prefer using : over <cmd> as the latter avoids going back in normal-mode.
--- see https://neovim.io/doc/user/map.html#:map-cmd
 local vmappings = {
   ["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle linewise (visual)" },
 }
 
 which_key.register(mappings, opts)
 which_key.register(vmappings, vopts)
-
--- local M = {}
--- M.config = function()
---   lvim.builtin.which_key = {
---     ---@usage disable which-key completely [not recommended]
---     active = true,
---     on_config_done = nil,
---     setup = {
---       plugins = {
---         marks = false, -- shows a list of your marks on ' and `
---         registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
---         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
---         -- No actual key bindings are created
---         presets = {
---           operators = false, -- adds help for operators like d, y, ...
---           motions = false, -- adds help for motions
---           text_objects = false, -- help for text objects triggered after entering an operator
---           windows = false, -- default bindings on <c-w>
---           nav = false, -- misc bindings to work with windows
---           z = false, -- bindings for folds, spelling and others prefixed with z
---           g = false, -- bindings for prefixed with g
---         },
---         spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
---       },
---       icons = {
---         breadcrumb = lvim.icons.ui.DoubleChevronRight, -- symbol used in the command line area that shows your active key combo
---         separator = lvim.icons.ui.BoldArrowRight, -- symbol used between a key and it's label
---         group = lvim.icons.ui.Plus, -- symbol prepended to a group
---       },
---       popup_mappings = {
---         scroll_down = "<c-d>", -- binding to scroll down inside the popup
---         scroll_up = "<c-u>", -- binding to scroll up inside the popup
---       },
---       window = {
---         border = "single", -- none, single, double, shadow
---         position = "bottom", -- bottom, top
---         margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
---         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
---         winblend = 0,
---       },
---       layout = {
---         height = { min = 4, max = 25 }, -- min and max height of the columns
---         width = { min = 20, max = 50 }, -- min and max width of the columns
---         spacing = 3, -- spacing between columns
---         align = "left", -- align columns left, center or right
---       },
---       hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
---       ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
---       show_help = true, -- show help message on the command line when the popup is visible
---       triggers = "auto", -- automatically setup triggers
---       -- triggers = {"<leader>"} -- or specify a list manually
---       triggers_blacklist = {
---         -- list of mode / prefixes that should never be hooked by WhichKey
---         -- this is mostly relevant for key maps that start with a native binding
---         -- most people should not need to change this
---         i = { "j", "k" },
---         v = { "j", "k" },
---       },
---     },
---   }
--- end
---
--- M.setup = function()
---   local which_key = require "which-key"
---
---   which_key.setup(lvim.builtin.which_key.setup)
---
---   which_key.register(mappings, opts)
---   which_key.register(vmappings, vopts)
--- end
