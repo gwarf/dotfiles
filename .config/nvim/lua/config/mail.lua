0fa0634b57e6822ca6b9f454e2d70d8f649ccf01
Baptiste Grenier <baptiste@bapt.name>
2022-11-26 18:15:33 +0100

    mail: clean conf, set formatoptions.

--- .config/nvim/lua/config/mail.lua
+++ .config/nvim/lua/config/mail.lua
@@ -1,9 +1,5 @@
 -- Setup to write mails with vim
---
--- Disable default mappings
-vim.g.VimMailDoNotMap = 0
--- Complete emails address in every files
--- vim.opt.completefunc = "vimmail#contacts#CompleteAddr"
+
 
 -- Enable spell checks using native spell checker
 -- setlocal spell
@@ -12,33 +8,27 @@ vim.g.VimMailDoNotMap = 0
 
 -- Automatic line wrap
 -- max line length
--- set textwidth=72
 -- :help fo-table
 -- set formatoptions=nawrjtcq
--- set fo=tcqnaw
+vim.opt.formatoptions = "tcqwan"
 -- set comments+=n:\|  -- '|' is a quote char.
 -- set comments+=n:% -- '%' as well.
 
--- Disable LessSpace for emails
--- let g:lessspace_enabled = 0
-
--- Disable automatic indent for emails
--- let b:sleuth_automatic = 0
-
 -- vim-mail
 -- https://github.com/dbeniamine/vim-mail/
+-- Use khard as a contact provider
+vim.g.VimMailContactsProvider = 'khard'
 -- Do not add custom keymaps
 -- XXX some are nice to move inside the file
 -- let g:VimMailDoNotMap=1
 -- let g:VimMailDoNotFold=1
 -- let g:VimMailSpellLangs=['en_gb', 'fr']
 -- Remove quoted signature is killing mine due to flow automatic formatting
---let g:VimMailStartFlags={'reply' :"boir" }
+-- let g:VimMailStartFlags={'reply' :"boir" }
 -- let g:VimMailStartFlags={
 -- \'blank': 'TAi',
 -- \'nosubject': 'SAi',
 -- \'default' : 'bOi'}
--- let g:VimMailContactsProvider=['khard']
 -- Use mu
 -- let g:VimMailContactsCommands={'mu' :
 --         \{ 'query' : "mu cfind",
@@ -52,12 +42,3 @@ vim.g.VimMailDoNotMap = 0
 --             \}
 --         \}
 --     \}
-
--- XXX Don't want to remove trailing spaces in emails!
--- XXX disabled while testing lessspace.vim
--- let g:ale_fix_on_save = 0
--- let g:ale_languagetool_options='--autoDetect --disable WORD_CONTAINS_UNDERSCORE,DASH_RULE,FRENCH_WITHESPACE'
-
--- Hide ALE error list window
--- let g:ale_open_list = 0
-
