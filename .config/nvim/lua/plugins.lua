b23e40deab520a58f4a5eebdfee6d30a5e014b3b
Baptiste Grenier <baptiste@bapt.name>
2022-11-26 20:35:32 +0100

    plugins.lua: install noive.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -457,8 +457,35 @@ return require("packer").startup(function(use)
       require("dressing").setup({})
     end,
   })
-  -- Notifications
-  use("rcarriga/nvim-notify")
+  -- Notifications, Messages, cmdline, pop up menu
+  use({
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+    requires = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  })
   -- use 'vigoux/notifier.nvim'
   -- a fast and fully programmable greeter for neovim.
   use({
