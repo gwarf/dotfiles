b6456a37d7eb26be072006d033c83e72b97a1e82
Baptiste Grenier <baptiste@bapt.name>
2022-11-26 16:31:43 +0100

    Drop default configuration

--- .config/nvim/lua/config/which-key.lua
+++ .config/nvim/lua/config/which-key.lua
@@ -4,37 +4,6 @@ local which_key = require("which-key")
 which_key.setup({
   window = {
     border = "single", -- none, single, double, shadow
-    -- position = "bottom", -- bottom, top
-    -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    -- padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    -- winblend = 0,
-  },
-  -- layout = {
-  --   height = { min = 4, max = 25 }, -- min and max height of the columns
-  --   width = { min = 20, max = 50 }, -- min and max width of the columns
-  --   spacing = 3, -- spacing between columns
-  --   align = "left", -- align columns left, center or right
-  -- },
-  plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
-  },
-  -- popup_mappings = {
-  --   scroll_down = "<c-d>", -- binding to scroll down inside the popup
-  --   scroll_up = "<c-u>", -- binding to scroll up inside the popup
-  -- },
-  -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-  show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
-  triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
-    i = { "j", "k" },
-    v = { "j", "k" },
   },
 })
 
