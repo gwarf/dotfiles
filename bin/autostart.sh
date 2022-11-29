ee75f9b3f1d4a5e70848259d59011106b6d38299
Baptiste Grenier <baptiste@bapt.name>
2022-11-29 21:41:16 +0100

    autostart: cleaning

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -11,28 +11,25 @@ run_once() {
   [ -z "$process_name" ] && process_name="$program"
 
   if [ -z "$program_arguments" ]; then
-    pgrep -f -u $USER -x "$process_name" || ( "$program" & )
+    pgrep -f -u "$USER" -x "$process_name" || ( "$program" & )
   else
-    pgrep -f -u $USER -x "$process_name" || ( "$program" "$program_arguments" & )
+    pgrep -f -u "$USER" -x "$process_name" || ( "$program" "$program_arguments" & )
   fi
 }
 
 ## OSD
 run_once dunst
 
-# Wallpapers
-#run_once nitrogen "--restore"
-
 # Disable offlineimap while testing a user systemd service + timer
 # pkill offlineimap || sleep 3 && offlineimap &
 #&& offlineimap-notify &> /tmp/offlineimap-log
 
 # Pulse audio
-pkill pulseaudio; sleep 3 && pulseaudio --start -D
+# pkill pulseaudio; sleep 3 && pulseaudio --start -D
 # pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo '60%' &
-run_once pasystray
-#exec --no-startup-id volti # starting volume control
-#exec --no-startup-id pa-applet
+# run_once pasystray
+# exec --no-startup-id volti # starting volume control
+# exec --no-startup-id pa-applet
 
 # MPD
 # run_once mpd
@@ -44,57 +41,44 @@ run_once pasystray
 # Clipboard manager
 # anamnesis, clipman, copyq, gpaste could be alternatives
 run_once clipit
-#
-# Start urxvt daemon
-#urxvtd -q -f -o & urxvtdpid=$!
-#urxvtd -f -o
 
 # Misc
 run_once kitty
 run_once firefox
+run_once keybase
 run_once redshift
 run_once redshift-gtk
-run_once keybase
-# run_once rofi "-key mod1+tab -terminal kitty"
 
 # Disabled
-# run_once revelation \
-#   ~/repos/perso/gwarf/private/revelation/keyring-perso \
-#   "/usr/bin/python.*/bin/revelation.*/keyring-perso"
-# run_once revelation \
-#   ~/repos/perso/gwarf/private/revelation/keyring-maatg \
-#   "/usr/bin/python.*/bin/revelation.*/keyring-maatg"
-#run_once JDownloader
-#run_once workrave
-#run_once chromium
-#run_once deluge
-#stalonetray &
-#tomboy &
-#alunn &
-#workrave &
-#nvidia-settings --load-config-only &
-#run_once syncthing-gtk
-#run_once owncloud
-#run_once flow
-#un_once kontaminuti
-#exec --no-startup-id udiskie
-#/usr/libexec/gnome-settings-daemon &
-#gnome-volume-manager &
-#x11vnc -ncache 10 -many -q -bg -rfbauth .vnc/passwd
+# run_once workrave
+# run_once chromium
+# run_once deluge
+# stalonetray &
+# tomboy &
+# alunn &
+# workrave &
+# nvidia-settings --load-config-only &
+# run_once syncthing-gtk
+# run_once flow
+# run_once kontaminuti
+# exec --no-startup-id udiskie
+# /usr/libexec/gnome-settings-daemon &
+# gnome-volume-manager &
+# x11vnc -ncache 10 -many -q -bg -rfbauth .vnc/passwd
 # run conky -c $HOME/.conkyrc &
 # albert &
 # run mpd &
-#run variety &
+# run variety &
 # run nm-applet &
-#pamac-tray &
+# pamac-tray &
 # xinput set-prop 13 302 1 &
 # run redshift-gtk
 # run syncthing-gtk -m &
-#xfce4-power-manager &
+# xfce4-power-manager &
 # run dunst &
 # numlockx on &
 # run blueberry-tray &
-#xfsettingsd &
+# xfsettingsd &
 # nitrogen --restore &
 # caffeine &
 # vivaldi-stable &
@@ -111,5 +95,6 @@ run_once keybase
 # wal -nRa 90 &
 
 # Set wallpaper
-# feh --bg-scale ~/.config/herbstluftwm/fantasy-landscape2.jpg &
-$HOME/.fehbg &
+# run_once nitrogen "--restore"
+# run_once feh --bg-scale ~/.config/herbstluftwm/fantasy-landscape2.jpg
+run_once feh --no-fehbg --bg-fill ~/.config/herbstluftwm/fantasy-landscape2.jpg
