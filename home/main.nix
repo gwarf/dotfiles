ea94500faeb01763ee5aa574b8fac4c167c36a36
gwarf <baptiste@bapt.name>
2022-12-03 20:34:00 +0100

    Install more packages

--- home/main.nix
+++ home/main.nix
@@ -13,6 +13,19 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # Bat, a substitute for cat.
+  # https://github.com/sharkdp/bat
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.bat.enable
+  programs.bat.enable = true;
+  programs.bat.config = {
+    style = "plain";
+  };
+
+  # Zoxide, a faster way to navigate the filesystem
+  # https://github.com/ajeetdsouza/zoxide
+  # https://rycee.gitlab.io/home-manager/options.html#opt-programs.zoxide.enable
+  programs.zoxide.enable = true;
+
   # Direnv, load and unload environment variables depending on the current directory.
   # https://direnv.net
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.direnv.enable
@@ -25,6 +38,7 @@
   programs.htop.settings.show_program_path = true;
 
   home.packages = with pkgs; [
+    # https://github.com/malob/nixpkgs/blob/d930a2e7749248a0b0af7d025d667030011fa7b8/home/packages.nix
     # Cool stuff
     gnupg
     mutt
@@ -32,8 +46,13 @@
     tmux
     bat
     exa
+    fd
+    ripgrep
     du-dust
+    tealdeer
     thefuck
+    command-not-found
+    xz
 
     # Dev stuff
     jq
