579cd96ce9a09459fe2e514c1437cc0b542db4f0
gwarf <baptiste@bapt.name>
2022-12-03 15:08:14 +0100

    Import conf from /etc/nixos/configuration.nix

--- .config/configuration-brutal.nix
+++ .config/configuration-brutal.nix
@@ -1,5 +1,95 @@
 { pkgs, lib, ... }:
 {
+  # Conf that was previously in /etc/nixos/configuration.nix
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration-brutal.nix
+    ];
+
+  # Use system-boot
+  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.consoleMode = "max";
+  boot.loader.systemd-boot.consoleMode = "keep";
+  networking.hostName = "brutal"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  # services.xserver.windowManager.herbstluftwm.enable = true;
+  services.xserver.displayManager.defaultSession = "none+i3";
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "caps:escape";  # map caps to escape.
+  # "eurosign:e";  # map caps to escape.
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.baptiste = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    # packages = with pkgs; [
+    #   firefox
+    # ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+  # Automatic upgrade
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
   # Nix configuration ------------------------------------------------------------------------------
   nix.settings.substituters = [
     "https://cache.nixos.org/"
@@ -24,21 +114,23 @@
   # Apps
   # `home-manager` currently has issues adding them to `~/Applications`
   # Issue: https://github.com/nix-community/home-manager/issues/1341
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    kitty
-    # XXX only needed for darwin
-    # terminal-notifier
-    git
-    wget
-    vim
     firefox
+    git
+    kitty
+    lm_sensors
+    mcelog
+    vim
+    wget
   ];
 
   # https://github.com/nix-community/home-manager/issues/423
   # environment.variables = {
   #   TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
   # };
-  programs.nix-index.enable = true;
+  # programs.nix-index.enable = true;
 
   # Fonts
   fonts.fontDir.enable = true;
@@ -50,8 +142,8 @@
    ];
 
   # Keyboard
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToEscape = true;
+  # system.keyboard.enableKeyMapping = true;
+  # system.keyboard.remapCapsLockToEscape = true;
 
   # Add ability to used TouchID for sudo authentication
   # security.pam.enableSudoTouchIdAuth = true;
