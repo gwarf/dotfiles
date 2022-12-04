0f8478094ad3f1057ae3df66b779f1f2ced17261
gwarf <baptiste@bapt.name>
2022-12-04 22:16:14 +0100

    i3: configure i3 using nix

--- home/i3.nix
+++ home/i3.nix
@@ -1,51 +1,68 @@
 { config, lib, pkgs, ... }:
 
-{
-  # programs.i3status-rust.enable = true;
+let
+  mod = "Mod1";
+in {
+  # https://github.com/srid/nix-config/blob/705a70c094da53aa50cf560179b973529617eb31/nix/home/i3.nix#L11
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = {
+      modifier = mod;
+      fonts = {
+        names = ["JetBrainsMono Nerd Font"];
+        size = 12.0;
+      };
+      keybindings = lib.mkOptionDefault {
+        "${mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+        "${mod}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+
+        # Focus
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+
+        # Move
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+      };
+      bars = [{
+          fonts = {
+            names = ["JetBrainsMono Nerd Font"];
+            size = 12.0;
+          };
+          position = "bottom";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
+     }];
+    };
+  };
+
   programs.i3status-rust = {
    enable = true;
    bars = {
      bottom = {
        # theme = (import ../gruvbox.nix).i3status-rust;
-       # icons = "awesome5";
+       icons = "awesome5";
        blocks = [
          {block = "cpu";}
+         {block = "temperature";}
          {block = "memory";}
          {block = "disk_space";}
-         #{ block = "nvidia_gpu"; }
-
-         #{ block = "docker"; }
-
-         {block = "net";}
          {
            block = "networkmanager";
            device_format = "{icon}{ap}";
+           on_click = "${pkgs.kitty}/bin/kitty -e nmtui";
          }
-
          {
            block = "time";
            format = "%F %a %R";
-         }
-         # {
-         #   block = "keyboard_layout";
-         #   driver = "sway";
-         #   mappings = {
-         #     "English (US)" = "EN";
-         #     # "Hebrew (N/A)" = "HE";
-         #   };
-         # }
-         # {block = "sound";}
-         {
-           block = "battery";
-           hide_missing = true;
+           timezone = "Europe/Paris";
+           locale = "fr_FR";
          }
        ];
      };
    };
   };
-  xsession.windowManager.i3.config.bars = [
-    {
-      status_command = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
-    }
-  ];
 }
