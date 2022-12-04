5e4c088cfb0460f176cf6e269db55c58f6aa82e0
gwarf <baptiste@bapt.name>
2022-12-04 22:00:11 +0100

    Install more fonts

--- configuration-brutal.nix
+++ configuration-brutal.nix
@@ -127,9 +127,11 @@
   fonts.fontDir.enable = true;
   # https://github.com/lightdiscord/nix-nerd-fonts-overlay
   # https://github.com/NixOS/nixpkgs/blob/6ba3207643fd27ffa25a172911e3d6825814d155/pkgs/data/fonts/nerdfonts/shas.nix#L2-L51
+  # https://github.com/JonathanReeve/dotfiles/blob/master/dotfiles/configuration.nix#L61
   fonts.fonts = with pkgs; [
      recursive
      (nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" ]; })
+     font-awesome_5
    ];
 
   # Keyboard
