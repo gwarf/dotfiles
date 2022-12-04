5f18058b9da8c52ab29ecfdd9aa8458f49b22491
gwarf <baptiste@bapt.name>
2022-12-04 23:08:40 +0100

    i3: more colors

--- home/i3.nix
+++ home/i3.nix
@@ -12,9 +12,62 @@ in {
         names = ["JetBrainsMono Nerd Font"];
         size = 12.0;
       };
+      # https://nix-community.github.io/home-manager/options.html#opt-xsession.windowManager.i3.config.colors
+      colors = {
+        background = "#F8F8F2";
+        focused = {
+          background = "#6272A4";
+          border = "#6272A4";
+          childBorder = "#6272A4";
+          indicator = "#6272A4";
+          text = "#F8F8F2";
+        };
+        focusedInactive = {
+          background = "#44475A";
+          border = "#44475A";
+          childBorder = "#44475A";
+          indicator = "#44475A";
+          text = "#F8F8F2";
+        };
+        unfocused = {
+          background = "#282A36";
+          border = "#282A36";
+          childBorder = "#282A36";
+          indicator = "#282A36";
+          text = "#BFBFBF";
+        };
+        urgent = {
+          background = "#FF5555";
+          border = "#44475A";
+          childBorder = "#FF5555";
+          indicator = "#FF5555";
+          text = "#F8F8F2";
+        };
+        placeholder = {
+          background = "#282A36";
+          border = "#282A36";
+          childBorder = "#282A36";
+          indicator = "#282A36";
+          text = "#F8F8F2";
+        };
+       # https://github.com/dracula/i3/blob/05ba49e6de572c037c0b9967d2dfc03186154fda/.config/i3/config
+       # bar {
+       #   colors {
+       #     background #282A36
+       #     statusline #F8F8F2
+       #     separator  #44475A
+
+       #     focused_workspace  #44475A #44475A #F8F8F2
+       #     active_workspace   #282A36 #44475A #F8F8F2
+       #     inactive_workspace #282A36 #282A36 #BFBFBF
+       #     urgent_workspace   #FF5555 #FF5555 #F8F8F2
+       #     binding_mode       #FF5555 #FF5555 #F8F8F2
+       #   }
+       # }
+      };
       keybindings = lib.mkOptionDefault {
         "${mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
-        "${mod}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+        "${mod}+p" = "exec ${pkgs.dmenu}/bin/dmenu_run -nf '#F8F8F2' -nb '#282A36' -sb '#6272A4' -sf '#F8F8F2' -fn 'monospace-10' -p 'dmenu%'";
 
         # Focus
         "${mod}+h" = "focus left";
