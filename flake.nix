d363555bd201074e08a461225d9521fa85b6bd75
gwarf <baptiste@bapt.name>
2022-12-06 13:37:42 +0100

    flake: cleaning

--- flake.nix
+++ flake.nix
@@ -1,18 +1,9 @@
 {
   # Inspirations
-  # - https://discourse.nixos.org/t/system-config-flake-with-darwin-and-linux-system-definitions/22343
   description = "Baptiste's systems";
 
   inputs = {
-    # Conf using unstable, nixos and nix darwin
-    # https://github.com/fmoda3/nix-configs/blob/3d640ab43d676a8aad555bcd29527345554252d0/flake.nix#L70
-    # https://github.com/vhsconnect/nixos-config/blob/08f47336b280e21fe360567bfd9c663bd5f1844c/flake.nix#L3
-    # https://github.com/Thrimbda/charles/blob/08bb883cff19b01c66bbe25c62c06545ccf40a1c/flake.nix
-    # https://github.com/ttak0422/ENV/blob/1478ba70e1a3eddd4dab1b65c191cc320e5b6cf1/flake.nix
-    # https://github.com/voidcontext/nix-config/blob/695668066e358be43298eab796f16232a09ba24e/flake.nix
-    # https://discourse.nixos.org/t/system-config-flake-with-darwin-and-linux-system-definitions/22343/3
     # Package sets
-    # XXX decide what to track (stable, master, unstable...)
     # XXX should we use master as nixpkgs on all systems?
     # nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
@@ -26,7 +17,6 @@
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs-darwin-stable";
-      # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # home-manager
@@ -44,9 +34,6 @@
     nur.url = "github:nix-community/NUR";
 
     nix2lua.url = "git+https://git.pleshevski.ru/mynix/nix2lua";
-
-    # flake-utils
-    # flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, darwin, nixpkgs, nixpkgs-darwin-stable, home-manager, ... }@inputs:
@@ -57,7 +44,7 @@
     homeStateVersion = "22.11";
 
     # Configuration for `nixpkgs`
-    nixpkgsConfig = with inputs; {
+    nixpkgsConfig = {
       config = { allowUnfree = true; };
       # overlays = [
       #   inputs.neovim-nightly-overlay.overlay
@@ -65,22 +52,7 @@
       # ];
     };
 
-    # XXX not used yet, to be used with flake-utils?
-    # from DPD-
-    machines = [
-      {
-        host = "brutal";
-        system = "x86_64-linux";
-        users = [ "baptiste" ];
-      }
-      {
-        host = "Baptiste-MBP";
-        system = "x86_64-darwin";
-        users = [ "baptiste" ];
-      }
-    ];
-
-    # Used for some modules
+    # Information about the main user
     primaryUserDefaults = {
        username = "baptiste";
        fullName = "Baptiste Grenier";
@@ -101,7 +73,6 @@
      my-starship = import ./home/starship.nix;
      my-starship-symbols = import ./home/starship-symbols.nix;
      my-mail = import ./home/mail.nix;
-     # XXX look at https://github.com/gvolpe/neovim-flake
      my-neovim = import ./home/neovim.nix;
      my-git = import ./home/git.nix;
      my-tmux = import ./home/tmux.nix;
@@ -112,7 +83,6 @@
 
      # Custom modules from gh:malob
      colors = import ./modules/home/colors;
-     # https://github.com/RichardYan314/dotfiles-nix/tree/c18de97ad9e7ae1ff46c806be2f0f91c43d5956e/users/colorschemes
      programs-neovim-extras = import ./modules/home/programs/neovim/extras.nix;
      programs-kitty-extras = import ./modules/home/programs/kitty/extras.nix;
      home-user-info = { lib, ... }: {
@@ -124,8 +94,6 @@
     nixosConfigurations = {
       brutal = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        # https://github.com/jules-goose/nixcfg/blob/0db16d98d049c1eb7c11f31c5ddbbcd2146e4f15/flake.nix#L22
-        inputs = { inherit home-manager nixpkgs; };
         pkgs = import nixpkgs {
           system = "x86_64-linux";
           overlays = [ inputs.nur.overlay ];
@@ -158,7 +126,7 @@
     darwinConfigurations = {
       Baptistes-MBP = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
-        # https://github.com/booklearner/nixconfig
+        # Includ darwin-sepcific inputs
         inputs = { inherit darwin home-manager nixpkgs nixpkgs-darwin-stable; };
         modules = [
           # Main `nix-darwin` config
