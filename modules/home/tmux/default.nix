b853c41402c52614c57e88d4f4e3cd5a4d58d792
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-12-08 11:59:17 +0100

    Move tmux to a dedicated module, start using new conf

---
+++ modules/home/tmux/default.nix
@@ -0,0 +1,85 @@
+# https://github.com/gvolpe/nix-config/blob/1c5ef00d2ecec075c4b7f78cddcde3598dd0c7a0/home/programs/tmux/default.nix
+{ config, pkgs, lib, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    baseIndex = 1;
+    escapeTime = 0;
+    keyMode = "vi";
+    # shell = "${pkgs.fish}/bin/fish";
+    plugins = with pkgs.tmuxPlugins; [
+      cpu
+      # nord # theme
+      dracula
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim "session"
+          set -g @dracula-show-powerline true
+          set -g @dracula-show-left-icon 💀
+          set -g @dracula-show-left-sep 
+          set -g @dracula-show-right-sep 
+          # set -g @dracula-border-contrast true
+          set -g @dracula-show-fahrenheit false
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60' # minutes
+        '';
+      }
+    ];
+    terminal = "screen-256color";
+    extraConfig = ''
+      set -g default-command ${pkgs.fish}/bin/fish
+      set -g default-shell ${pkgs.fish}/bin/fish
+      # automatically renumber tmux windows
+      set -g renumber-windows on
+
+      # status bar at the top of the screen
+      set-option -g status-position "top"
+
+      # Activity Monitoring
+      setw -g monitor-activity off
+      set -g visual-activity off
+
+      ############ Key Bindings ############
+
+      # reload config file
+      bind r source-file ~/.tmux.conf \; display "Config Reloaded!"
+
+      # split window and fix path for tmux 1.9
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      # synchronize all panes in a window
+      bind y setw synchronize-panes
+
+      # pane movement shortcuts
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      bind -r C-h select-window -t :-
+      bind -r C-l select-window -t :+
+
+      # Resize pane shortcuts
+      bind -r H resize-pane -L 10
+      bind -r J resize-pane -D 10
+      bind -r K resize-pane -U 10
+      bind -r L resize-pane -R 10
+
+      # Remap x to avoid confirmation to kill pane
+      bind-key x kill-pane
+
+      # Move windows
+      bind-key S-Left swap-window -t -1\; select-window -t -1
+      bind-key S-Right swap-window -t +1\; select-window -t +1
+    '';
+  };
+}
