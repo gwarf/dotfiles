13bb395a9a8cf44934af17dbdb8a8d30d42a6667
Baptiste Grenier <baptiste.grenier@egi.eu>
2022-12-09 09:47:16 +0100

    Load macOS conf as a module, start handling some packages with homebrew.

---
+++ machines/Baptistes-MBP/default.nix
@@ -0,0 +1,28 @@
+{ lib, ... }:
+{
+  imports = [
+    ./configuration-Baptistes-MBP.nix
+    ../../modules/system/yabai
+  ];
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    # updates homebrew packages on activation,
+    # can make darwin-rebuild much slower
+    onActivation.upgrade = true;
+    brews = [
+      "terminal-notifier"
+      "the_silver_searcher"
+
+      # mail setup
+      # XXX to be moved to a module
+      "isync"
+      "mutt"
+      "msmtp"
+      "gpgme"
+
+      # XXX install from pkgs does not work
+      "joplin-cli"
+    ];
+};
+}
