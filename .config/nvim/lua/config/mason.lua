5313bd59ebe43337e6ff019960f49feef6774f8f
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-01-11 10:50:47 +0100

    Rely on nix for installing packages.

--- .config/nvim/lua/config/mason.lua
+++ .config/nvim/lua/config/mason.lua
@@ -2,44 +2,46 @@ require("mason").setup()
 
 require("mason-tool-installer").setup({
   ensure_installed = {
-    "alex",
-    "actionlint",
-    "black",
-    "beautysh",
-    "isort",
+    -- XXX now installed via nix
+    -- "alex",
+    -- "actionlint",
+    -- "black",
+    -- "beautysh",
+    -- "isort",
     -- "jedi-language-server",
-    "stylua",
-    "markdownlint",
-    "pylint",
-    "prettier",
-    "shellcheck",
-    "shfmt",
+    -- "stylua",
+    -- "markdownlint",
+    -- "pylint",
+    -- "prettier",
+    -- "shellcheck",
+    -- "shfmt",
   },
-  auto_update = true,
-  run_on_start = true,
+  auto_update = false,
+  run_on_start = false,
   tart_delay = 3000, -- 3 second delay
 })
 
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "ansiblels",
-    "bashls",
+    -- XXX now installed via nix
+    -- "ansiblels",
+    -- "bashls",
     -- "grammarly",
     --  "html",
-    "jsonls",
-    --  "lemminx",
-    "ltex",
-    "marksman",
+    -- "jsonls",
+    -- "lemminx",
+    -- "ltex",
+    -- "marksman",
     --  "prosemd_lsp",
     --  Struggling with plugins and pylint
     --  https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/server_configurations/pylsp/README.md
     -- "pylsp",
-    "pyright",
-    "rnix",
-    --  "ruby_ls",
-    "sumneko_lua",
-    "vimls",
-    "yamlls",
+    -- "pyright",
+    -- "rnix",
+    -- "ruby_ls",
+    -- "sumneko_lua",
+    -- "vimls",
+    -- "yamlls",
   },
-  automatic_installation = true,
+  automatic_installation = false,
 })
