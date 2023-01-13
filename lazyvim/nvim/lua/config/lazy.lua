d15c5a53cc80cbee2556e85c51564c211d61fc38
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-01-13 10:12:06 +0100

    Do not install habamax colorscheme

--- lazyvim/nvim/lua/config/lazy.lua
+++ lazyvim/nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     lazy = true, -- every plugin is lazy-loaded by default
     version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
