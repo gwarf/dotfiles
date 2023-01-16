b9b00f1558697f9bb2f3405671e487a988ee8333
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-01-16 10:06:44 +0100

    nvim: customize indentation, add mappings for command mode.

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- Go to beginning and end of of command in command-line mode
+keymap.set("c", "<C-A>", "<HOME>")
+keymap.set("c", "<C-E>", "<END>")
