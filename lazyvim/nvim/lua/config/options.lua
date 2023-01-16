edee5597d335281dd64d73624ae5ab627343f816
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-01-16 10:06:44 +0100

    nvim: customize indentation, add mappings for command mode.

--- lazyvim/nvim/lua/config/options.lua
+++ lazyvim/nvim/lua/config/options.lua
@@ -1,4 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- local opt = vim.opt
+local opt = vim.opt
+
+-- Default indentation: 2 spaces
+opt.softtabstop = 2
+-- XXX breaking indentation of new comments
+-- https://unix.stackexchange.com/a/543571
+opt.smartindent = false
+opt.autoindent = false
+-- Indent wrapped lines to match line start
+opt.breakindent = true
+-- Fix indentation of new comments
+opt.cinkeys:remove({ "0#" })
