4b58c329c6bbc2f269bdd543a92434adc7d49bee
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-07 09:44:58 +0100

    kitty: back to tokyonight_storm.

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -37,8 +37,8 @@
 
   programs.kitty.extraConfig = ''
     # Load theme
-    # include tokyonight_storm.conf
-    include tokyonight_moon.conf
+    include tokyonight_storm.conf
+    # include tokyonight_moon.conf
     # include dracula.conf
 
     # Change the style of italic font variants
@@ -49,4 +49,5 @@
     symbol_map = "U+E5FA-U+E62B,U+E700-U+E7C5,U+F000-U+F2E0,U+E200-U+E2A9,U+F500-U+FD46,U+E300-U+E3EB,U+F400-U+F4A8,U+2665,U+26a1,U+F27C,U+E0A3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D2,U+E0D4,U+23FB-U+23FE,U+2B58,U+F300-U+F313,U+E000-U+E00D JetBrainsMono Nerd Font";
   '';
 }
+
 # vim: foldmethod=marker
