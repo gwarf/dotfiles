d59b2d58c1842937bf1f9bf72cec64ea604637e7
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-09 18:15:29 +0100

    neovim: disable virtual text for LSP, show it on hover. Fix ltex configuration.

--- lazyvim/nvim/lua/config/autocmds.lua
+++ lazyvim/nvim/lua/config/autocmds.lua
@@ -43,3 +43,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.wo.conceallevel = 0
   end,
 })
+
+-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = "rounded",
+      source = "always",
+      prefix = " ",
+      scope = "cursor",
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end,
+})
