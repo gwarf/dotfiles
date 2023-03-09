cab1991cb18406c94b376cf7dd7e2204650213b7
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-09 18:14:54 +0100

    neovim: disable copilot.

--- lazyvim/nvim/lua/plugins/coding.lua
+++ lazyvim/nvim/lua/plugins/coding.lua
@@ -223,16 +223,16 @@ return {
   },
 
   -- copilot
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    },
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   enabled = true,
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --   },
+  -- },
 
   {
     "simrat39/symbols-outline.nvim",
@@ -244,15 +244,16 @@ return {
     "nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
-      {
-        "zbirenbaum/copilot-cmp",
-        opts = {},
-      },
+      -- {
+      --   "zbirenbaum/copilot-cmp",
+      --   opts = {},
+      -- },
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
+      -- opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
     end,
   },
 }
