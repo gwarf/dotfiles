ea47b0788b6cc0557c57c40febc72fdc6903065b
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-09 19:35:33 +0100

    Document commenting.

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ local keymap = vim.keymap
 -- Go to beginning and end of of command in command-line mode
 keymap.set("c", "<C-A>", "<HOME>")
 keymap.set("c", "<C-E>", "<END>")
+
+-- Commenting is done using https://github.com/echasnovski/mini.comment
+-- use gc in normal/visual mode
+-- use gcc to toggle current line
