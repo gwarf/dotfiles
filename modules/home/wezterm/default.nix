00b2d27fc2b07daf0a9638b746219b5732fff2bd
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-23 11:56:58 +0100

    Import module for wezterm

---
+++ modules/home/wezterm/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, pkgs-unstable, ... }:
+{
+  programs.wezterm.enable = true;
+  programs.wezterm.package = pkgs-unstable.wezterm;
+  programs.wezterm.extraConfig = ''
+    return {
+      font = wezterm.font_with_fallback({ "JetBrains Mono", "Material Design Icons", "Material Icons", "Noto Color Emoji" })
+      font_size = 16.0
+      -- FIXME: themes not found, only availalbe in most recent wezterm versions
+      -- color_scheme = "Tokyo Night Storm (Gogh)"
+      -- color_scheme = "tokyonight_moon"
+      -- color_scheme = "tokyonight_storm"
+      color_scheme = "Dracula"
+      hide_tab_bar_if_only_one_tab = true
+      default_prog = { "${pkgs.fish}/bin/fish", "-l" }
+    }
+  '';
+}
+# vim: foldmethod=marker
