8ad4b1a0909a28e47835af9348683eedcd03db7e
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-04-04 10:27:33 +0200

    wezterm: fix setting conf from nix.

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -4,15 +4,15 @@
   programs.wezterm.package = pkgs-unstable.wezterm;
   programs.wezterm.extraConfig = ''
     return {
-      font = wezterm.font_with_fallback({ "JetBrains Mono", "Material Design Icons", "Material Icons", "Noto Color Emoji" })
-      font_size = 16.0
-      -- FIXME: themes not found, only availalbe in most recent wezterm versions
-      -- color_scheme = "Tokyo Night Storm (Gogh)"
-      -- color_scheme = "tokyonight_moon"
-      -- color_scheme = "tokyonight_storm"
-      color_scheme = "Dracula"
-      hide_tab_bar_if_only_one_tab = true
-      default_prog = { "${pkgs.fish}/bin/fish", "-l" }
+      font = wezterm.font_with_fallback({ "JetBrains Mono", "Material Design Icons", "Material Icons", "Noto Color Emoji" }),
+      font_size = 16.0,
+      -- FIXME: themes not found, only availalbe in most recent wezterm versions,
+      -- color_scheme = "Tokyo Night Storm (Gogh)",
+      -- color_scheme = "tokyonight_moon",
+      -- color_scheme = "tokyonight_storm",
+      color_scheme = "Dracula",
+      hide_tab_bar_if_only_one_tab = true,
+      default_prog = { "${pkgs.fish}/bin/fish", "-l" },
     }
   '';
 }
