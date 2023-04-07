db0d9f0601743002d0a069ff55d3ed6029f4b022
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-04-07 15:32:29 +0200

    lazy: disable auto update

--- lazyvim/nvim/lua/config/lazy.lua
+++ lazyvim/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
     version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "catppuccin" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   -- diff = {
   --   cmd = "terminal_git",
   -- },
