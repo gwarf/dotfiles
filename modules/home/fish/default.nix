f026f3f8ac9ecae2167ddf0c45017443f7794c38
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-05-10 09:02:26 +0200

    wrap nvim in a function to set proper CC for neorg

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -130,7 +130,12 @@ in
       # set -gx CC clang
       # set -gx CXX clang++
       # set -gx COMPILER clang++
-      set -gx CC gcc-12
+      # set -gx CC gcc-12
+
+      # Use clang++ from system with recent C++ version to build neorg treesitter
+      function nvim
+        env CC="/usr/bin/clang++ -std=c++17" /etc/profiles/per-user/$USER/bin/nvim $argv
+      end
 
       # Load pyenv
       # https://github.com/pyenv/pyenv/wiki/Common-build-problems
