f15797b9fc687f1bbd0b1760343e9206f17f0985
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-08-01 11:25:03 +0200

    neorg: update doc, extend conf

--- lazyvim/nvim/lua/plugins/neorg.lua
+++ lazyvim/nvim/lua/plugins/neorg.lua
@@ -1,11 +1,16 @@
 return {
   -- neorg, org-mode like
   -- https://github.com/nvim-neorg/neorg
-  -- Bindings: ~/.local/share/nvim/lazy/neorg/lua/neorg/modules/core/keybinds/keybinds.lua
+  -- Bindings mapped via localleader (\)
+  -- ~/.local/share/nvim/lazy/neorg/lua/neorg/modules/core/keybinds/keybinds.lua
   {
     "nvim-neorg/neorg",
     -- lazy-load on filetype
+    enabled = true,
     ft = "norg",
+    build = ":Neorg sync-parsers",
+    cmd = "Neorg",
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       load = {
         -- Load default modules
