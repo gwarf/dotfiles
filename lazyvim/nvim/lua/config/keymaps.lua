f07d0a2082aabd90804a11ef20c32cb9ae961980
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-08-01 11:23:34 +0200

    keymaps: more movement from Folke.

--- lazyvim/nvim/lua/config/keymaps.lua
+++ lazyvim/nvim/lua/config/keymaps.lua
@@ -3,6 +3,12 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 
+-- Move to window using the movement keys
+keymap.set("n", "<left>", "<C-w>h")
+keymap.set("n", "<down>", "<C-w>j")
+keymap.set("n", "<up>", "<C-w>k")
+keymap.set("n", "<right>", "<C-w>l")
+
 -- Go to beginning and end of of command in command-line mode
 keymap.set("c", "<C-A>", "<HOME>")
 keymap.set("c", "<C-E>", "<END>")
