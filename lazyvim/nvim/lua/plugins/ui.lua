fdb92508bb54ba5aba33995256493d7c81e6f39d
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-10-02 09:28:07 +0200

    lazyvim: drop zen mode, unused.

--- lazyvim/nvim/lua/plugins/ui.lua
+++ lazyvim/nvim/lua/plugins/ui.lua
@@ -157,20 +157,6 @@ return {
   -- dims inactive portions of the code
   { "folke/twilight.nvim" },
 
-  -- Zen mode for focusing on editing code
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    opts = {
-      plugins = {
-        gitsigns = true,
-        tmux = true,
-        kitty = { enabled = false, font = "+2" },
-      },
-    },
-    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-  },
-
   -- Fold management
   {
     "kevinhwang91/nvim-ufo",
