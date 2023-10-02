9cedf143fcaf967a9ff88d8772b4db97e3919aa2
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-10-02 09:30:54 +0200

    lazyvim: drop commented code.

--- lazyvim/nvim/lua/plugins/ui.lua
+++ lazyvim/nvim/lua/plugins/ui.lua
@@ -38,15 +38,6 @@ return {
           hl.LineNr = { bold = true }
           hl.LineNrAbove = { fg = c.fg_gutter }
           hl.LineNrBelow = { fg = c.fg_gutter }
-          -- XXX: disabled to test vanilla configuration
-          -- local prompt = "#2d3149"
-          -- hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-          -- hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-          -- hl.TelescopePromptNormal = { bg = prompt }
-          -- hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-          -- hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
-          -- hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
-          -- hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
           hl.WinSeparator = { fg = c.blue }
         end,
       }
