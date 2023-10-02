e858df30eb70f406897e027d06c9da82ea87d8e3
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-10-02 15:32:08 +0200

    Drop legacy neovim config

--- configs/nvim/lua/malo/nvim-treesitter.lua
+++
@@ -1,9 +0,0 @@
--- nvim-treesitter
--- https://github.com/nvim-treesitter/nvim-treesitter
-vim.cmd 'packadd nvim-treesitter'
-
-require'nvim-treesitter.configs'.setup {
-  highlight = { enable = true },
-  incremental_selection = { enable = true },
-  indent = { enable = true },
-}
