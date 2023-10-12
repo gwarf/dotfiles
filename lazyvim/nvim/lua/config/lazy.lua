2e0024048dfa0d9155ba7ac20342d5254c219ae1
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-10-12 16:04:38 +0200

    LazyVim: update to 10.0

--- lazyvim/nvim/lua/config/lazy.lua
+++ lazyvim/nvim/lua/config/lazy.lua
@@ -33,12 +33,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.dot" },
     -- copilot disabled until there is an easy way to opt in
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
-    -- Extra for formatting, replacing none-ls
-    { import = "lazyvim.plugins.extras.formatting.conform" },
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- Extra for linting, replacing none-ls
-    { import = "lazyvim.plugins.extras.linting.nvim-lint" },
     -- import/override with your plugins
     { import = "plugins" },
   },
