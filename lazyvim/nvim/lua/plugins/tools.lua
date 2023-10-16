fe9a174d451e26e20545d67d56ca98282d0b8453
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-10-16 10:07:39 +0200

    lazyvim: switch to extra for markdown configuration.

--- lazyvim/nvim/lua/plugins/tools.lua
+++ lazyvim/nvim/lua/plugins/tools.lua
@@ -41,27 +41,6 @@ return {
     },
   },
 
-  -- markdown preview
-  {
-    "toppair/peek.nvim",
-    build = "deno task --quiet build:fast",
-    keys = {
-      {
-        "<leader>op",
-        function()
-          local peek = require("peek")
-          if peek.is_open() then
-            peek.close()
-          else
-            peek.open()
-          end
-        end,
-        desc = "Peek (Markdown Preview)",
-      },
-    },
-    opts = { theme = "light", app = "browser" },
-  },
-
   -- better diffing
   {
     "sindrets/diffview.nvim",
