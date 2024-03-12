81b7d730265a1d59b0aa74dcd6c6ed4dee5d07f8
gwarf <baptiste@bapt.name>
2024-03-12 09:19:22 +0100

    Fix deprecation warning.

--- machines/brutal/configuration.nix
+++ machines/brutal/configuration.nix
@@ -180,7 +180,7 @@
   # https://github.com/lightdiscord/nix-nerd-fonts-overlay
   # https://github.com/NixOS/nixpkgs/blob/6ba3207643fd27ffa25a172911e3d6825814d155/pkgs/data/fonts/nerdfonts/shas.nix#L2-L51
   # https://github.com/JonathanReeve/dotfiles/blob/master/dotfiles/configuration.nix#L61
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     # Only pick selected fonts
     (nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" ]; })
     fantasque-sans-mono
