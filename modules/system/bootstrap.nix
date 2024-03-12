f18196c1a6536743bac7f98ab056582680ba6bb0
gwarf <baptiste@bapt.name>
2024-03-12 09:20:08 +0100

    Fix types.string deprecation
    https://github.com/NixOS/nixpkgs/pull/66346

--- modules/system/bootstrap.nix
+++ modules/system/bootstrap.nix
@@ -44,7 +44,7 @@
   # Needed to address bug where $PATH is not properly set for fish:
   # https://github.com/LnL7/nix-darwin/issues/122
   programs.fish.shellInit = ''
-    for p in (string split : ${config.environment.systemPath})
+    for p in (str split : ${config.environment.systemPath})
       if not contains $p $fish_user_paths
         set -g fish_user_paths $fish_user_paths $p
       end
