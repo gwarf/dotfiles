e0f62d4e535b5ef14297a3eb0ec7df5c5613400f
Baptiste Grenier <baptiste.grenier@egi.eu>
2024-05-21 17:48:58 +0200

    Update nvim-colorizer conf

--- lazyvim/nvim/lua/plugins/tools.lua
+++ lazyvim/nvim/lua/plugins/tools.lua
@@ -42,7 +42,15 @@ return {
   {
     "NvChad/nvim-colorizer.lua",
     config = function()
-      require("colorizer").setup({ filetypes = { "*" } })
+      require("colorizer").setup({
+        filetypes = { "*" },
+        cmp_docs = {
+          always_update = true,
+        },
+        cmp_menu = {
+          always_update = true,
+        },
+      })
     end,
   },
 
