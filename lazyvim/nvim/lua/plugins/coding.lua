eb4b9f8b5613106a48af2bac27df8dc7af9669b6
Baptiste Grenier <baptiste.grenier@egi.eu>
2024-05-21 17:59:56 +0200

    lazyvim: drop dial.nvim manual conf as now there is an extra

--- lazyvim/nvim/lua/plugins/coding.lua
+++ lazyvim/nvim/lua/plugins/coding.lua
@@ -119,30 +119,6 @@ return {
     opts = {},
   },
 
-  -- better increase/descrease
-  {
-    "monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group({
-        default = {
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          ---@diagnostic disable-next-line: assign-type-mismatch
-          augend.date.alias["%Y/%m/%d"],
-          ---@diagnostic disable-next-line: assign-type-mismatch
-          augend.constant.alias.bool,
-          augend.semver.alias.semver,
-        },
-      })
-    end,
-  },
-
   -- Use <tab> for completion and snippets (supertab).
   {
     "L3MON4D3/LuaSnip",
