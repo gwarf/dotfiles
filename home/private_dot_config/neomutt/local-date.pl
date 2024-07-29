5b31b4419b6c1d1b046ce7544135c80ab18869b9
Baptiste Grenier <baptiste.grenier@egi.eu>
2024-07-29 15:39:51 +0200

    chezmoi: merge updates from Darwin

--- home/private_dot_config/neomutt/local-date.pl
+++ home/private_dot_config/neomutt/local-date.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/env -S perl -n
 
+# FIXME: when showing full headers we shouldn't change them
+
 use Date::Parse;
 use POSIX;
 use strict;
 
-if (/^Date: (.*)$/) {
-    my $datestr = $1;
-    my $date = strftime ("%a, %d %b %Y %H:%M:%S",
-                         localtime (str2time ($datestr)));
-    print "Local-Date: $date\n";
+while (<>) {
+  if (/^Date: (.*)$/) {
+      my $datestr = $1;
+      my $date = strftime ("%a, %d %b %Y %H:%M:%S",
+                          localtime (str2time ($datestr)));
+      print "Date: $date\n";
+  } else {
+    print;
+  }
 }
-
-print;
