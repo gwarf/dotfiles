bb1987c0c30ee115c2465cc4c1876ad2bcb7702b
Baptiste Grenier <baptiste.grenier@egi.eu>
2025-01-06 15:40:28 +0100

    nvim: cleanup blink + lbdb setup, still broken.

--- home/private_dot_config/nvim/lua/plugins/mail.lua
+++ home/private_dot_config/nvim/lua/plugins/mail.lua
@@ -1,68 +1,27 @@
 -- Loading CMP source in blink using https://github.com/Saghen/blink.compat
 -- derived from https://github.com/LazyVim/LazyVim/blob/c1ee761dd88ec71fa9c9eb9706828598e7522c5d/lua/lazyvim/plugins/extras/ai/tabnine.lua#L40
--- FIXME: configuration à la LazyVim
--- Not working
+-- https://github.com/Saghen/blink.compat/issues/19
 return {
-
-  { "codybuell/cmp-lbdb" },
   {
     "saghen/blink.cmp",
-    -- optional = true,
     dependencies = {
-      "codybuell/cmp-lbdb",
+      -- works
+      "hrsh7th/cmp-emoji",
+      -- FIXME: does not work
+      {
+        "codybuell/cmp-lbdb",
+        -- lazy = true,
+        -- ft = "mail",
+        -- opts = {
+        --   mail_header_only = false,
+        -- },
+      },
       "saghen/blink.compat",
     },
     opts = {
       sources = {
-        compat = { "lbdb" },
-        providers = {
-          lbdb = {
-            name = "lbdb",
-          },
-        },
+        compat = { "lbdb", "emoji" },
       },
     },
   },
 }
-
--- FIXME: native configuration
--- not working too
--- return {
---   -- add blink.compat
---   -- {
---   --   "saghen/blink.compat",
---   --   lazy = true,
---   --   opts = {},
---   -- },
---
---   {
---     "saghen/blink.cmp",
---     dependencies = {
---       -- add source
---       { "codybuell/cmp-lbdb" },
---     },
---     sources = {
---       completion = {
---         -- remember to enable your providers here
---         enabled_providers = { "lsp", "path", "snippets", "buffer", "lbdb" },
---       },
---
---       providers = {
---         -- create provider
---         lbdb = {
---           name = "lbdb",
---           module = "blink.compat.source",
---
---           -- all blink.cmp source config options work as normal:
---           score_offset = -3,
---
---           -- this table is passed directly to the proxied completion source
---           -- as the `option` field in nvim-cmp's source config
---           --
---           -- this is NOT the same as the opts in a plugin's lazy.nvim spec
---           opts = {},
---         },
---       },
---     },
---   },
--- }
