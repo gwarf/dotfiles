a194cb2400a24beea06e8d30d9afac7094e39dfe
Baptiste Grenier <baptiste.grenier@egi.eu>
2025-01-27 11:54:22 +0100

    nvim: disable blink-related conf only on FreeBSD

--- home/private_dot_config/nvim/lua/plugins/mail.lua
+++ home/private_dot_config/nvim/lua/plugins/mail.lua
@@ -1,27 +1,29 @@
 -- Loading CMP source in blink using https://github.com/Saghen/blink.compat
 -- derived from https://github.com/LazyVim/LazyVim/blob/c1ee761dd88ec71fa9c9eb9706828598e7522c5d/lua/lazyvim/plugins/extras/ai/tabnine.lua#L40
 -- https://github.com/Saghen/blink.compat/issues/19
-return {
-  -- {
-  --   "saghen/blink.cmp",
-  --   dependencies = {
-  --     -- works
-  --     "hrsh7th/cmp-emoji",
-  --     -- FIXME: does not work
-  --     {
-  --       "codybuell/cmp-lbdb",
-  --       -- lazy = true,
-  --       -- ft = "mail",
-  --       -- opts = {
-  --       --   mail_header_only = false,
-  --       -- },
-  --     },
-  --     "saghen/blink.compat",
-  --   },
-  --   opts = {
-  --     sources = {
-  --       compat = { "lbdb", "emoji" },
-  --     },
-  --   },
-  -- },
-}
+if not vim.uv.os_uname().sysname:find("FreeBSD") then
+  return {
+    {
+      "saghen/blink.cmp",
+      dependencies = {
+        -- works
+        "hrsh7th/cmp-emoji",
+        -- FIXME: does not work
+        {
+          "codybuell/cmp-lbdb",
+          -- lazy = true,
+          -- ft = "mail",
+          -- opts = {
+          --   mail_header_only = false,
+          -- },
+        },
+        "saghen/blink.compat",
+      },
+      opts = {
+        sources = {
+          compat = { "lbdb", "emoji" },
+        },
+      },
+    },
+  }
+end
--- home/private_dot_config/nvim/lua/plugins/obsidian.lua
+++ home/private_dot_config/nvim/lua/plugins/obsidian.lua
@@ -3,49 +3,51 @@
 -- https://github.com/Gentleman-Programming/Gentleman.Dots/blob/main/GentlemanNvim/nvim/lua/plugins/obsidian.lua
 -- https://github.com/epwalsh/obsidian.nvim/issues/770
 -- https://github.com/maxclax/dotfiles/blob/main/private_dot_config/nvim-custom/lua/plugins/obsidian.lua.tmpl
-return {
-  -- -- Blink Auto-completion for Obsidian (replace nvim_cmp)
-  -- {
-  --   "saghen/blink.cmp",
-  --   dependencies = {
-  --     {
-  --       "epwalsh/obsidian.nvim",
-  --       version = "*", -- recommended, use latest release instead of latest commit
-  --       lazy = true,
-  --       ft = "markdown",
-  --       -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  --       -- event = {
-  --       --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --       --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --       --   "BufReadPre path/to/my-vault/**.md",
-  --       --   "BufNewFile path/to/my-vault/**.md",
-  --       -- },
-  --       dependencies = {
-  --         "nvim-lua/plenary.nvim",
-  --       },
-  --       opts = {
-  --         ui = { enable = false },
-  --         workspaces = {
-  --           {
-  --             name = "Main",
-  --             path = "~/Obsidian/Main",
-  --           },
-  --         },
-  --         completion = {
-  --           -- Set to false to disable completion.
-  --           nvim_cmp = false,
-  --           -- Trigger completion at 2 chars.
-  --           min_chars = 2,
-  --         },
-  --       },
-  --     },
-  --     "saghen/blink.compat",
-  --   },
-  --   opts = {
-  --     -- LazyVim as custom option compat to pass in external sources with blink.compat
-  --     sources = {
-  --       compat = { "obsidian", "obsidian_new", "obsidian_tags" },
-  --     },
-  --   },
-  -- },
-}
+if not vim.uv.os_uname().sysname:find("FreeBSD") then
+  return {
+    -- Blink Auto-completion for Obsidian (replace nvim_cmp)
+    {
+      "saghen/blink.cmp",
+      dependencies = {
+        {
+          "epwalsh/obsidian.nvim",
+          version = "*", -- recommended, use latest release instead of latest commit
+          lazy = true,
+          ft = "markdown",
+          -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+          -- event = {
+          --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+          --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+          --   "BufReadPre path/to/my-vault/**.md",
+          --   "BufNewFile path/to/my-vault/**.md",
+          -- },
+          dependencies = {
+            "nvim-lua/plenary.nvim",
+          },
+          opts = {
+            ui = { enable = false },
+            workspaces = {
+              {
+                name = "Main",
+                path = "~/Obsidian/Main",
+              },
+            },
+            completion = {
+              -- Set to false to disable completion.
+              nvim_cmp = false,
+              -- Trigger completion at 2 chars.
+              min_chars = 2,
+            },
+          },
+        },
+        "saghen/blink.compat",
+      },
+      opts = {
+        -- LazyVim as custom option compat to pass in external sources with blink.compat
+        sources = {
+          compat = { "obsidian", "obsidian_new", "obsidian_tags" },
+        },
+      },
+    },
+  }
+end
