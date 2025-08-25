7fb3221909c678a4334a90f021bf473527dfd950
Baptiste Grenier <baptiste.grenier@operas-eu.org>
2025-08-25 14:31:13 +0200

    feat(nvim): disable CodeCompanion while testing copilot

--- home/private_dot_config/nvim/lua/plugins/gemini.lua
+++ home/private_dot_config/nvim/lua/plugins/gemini.lua
@@ -8,48 +8,48 @@ return {
   --     require("gemini").setup()
   --   end,
   -- },
-  {
-    "olimorris/codecompanion.nvim",
-    cmd = {
-      "CodeCompanion",
-      "CodeCompanionActions",
-      "CodeCompanionChat",
-      "CodeCompanionCmd",
-    },
-    opts = {
-      strategies = {
-        chat = {
-          adapter = "gemini",
-        },
-        inline = {
-          adapter = "gemini",
-        },
-      },
-      gemini = function()
-        return require("codecompanion.adapters").extend("gemini", {
-          schema = {
-            model = {
-              default = "gemini-2.5-flash-preview-05-20",
-            },
-          },
-          env = {
-            api_key = "GEMINI_API_KEY",
-          },
-        })
-      end,
-      display = {
-        diff = {
-          provider = "mini_diff",
-        },
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        ft = { "codecompanion" },
-      },
-    },
-  },
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   cmd = {
+  --     "CodeCompanion",
+  --     "CodeCompanionActions",
+  --     "CodeCompanionChat",
+  --     "CodeCompanionCmd",
+  --   },
+  --   opts = {
+  --     strategies = {
+  --       chat = {
+  --         adapter = "gemini",
+  --       },
+  --       inline = {
+  --         adapter = "gemini",
+  --       },
+  --     },
+  --     gemini = function()
+  --       return require("codecompanion.adapters").extend("gemini", {
+  --         schema = {
+  --           model = {
+  --             default = "gemini-2.5-flash-preview-05-20",
+  --           },
+  --         },
+  --         env = {
+  --           api_key = "GEMINI_API_KEY",
+  --         },
+  --       })
+  --     end,
+  --     display = {
+  --       diff = {
+  --         provider = "mini_diff",
+  --       },
+  --     },
+  --   },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     {
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       ft = { "codecompanion" },
+  --     },
+  --   },
+  -- },
 }
