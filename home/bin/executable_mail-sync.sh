981c269e6bed834b71d544d5503cbef3b23ee695
Baptiste Grenier <baptiste.grenier@operas-eu.org>
2025-11-07 15:33:04 +0100

    feat(mail): fix afew/aerc setup

--- home/bin/executable_mail-sync.sh
+++ home/bin/executable_mail-sync.sh
@@ -4,8 +4,8 @@ MBSYNC=$(pgrep mbsync)
 NOTMUCH=$(pgrep notmuch)
 
 if [ -n "$MBSYNC" ] || [ -n "$NOTMUCH" ]; then
-    echo "Already running one instance of mbsync or notmuch. Exiting..."
-    exit 0
+  echo "Already running one instance of mbsync or notmuch. Exiting..."
+  exit 0
 fi
 
 # Move tagged messages to the related folders (i.e. spam and junk)
@@ -13,6 +13,11 @@ afew --move --all --verbose
 
 echo "Deleting messages tagged as *deleted*"
 notmuch search --format=text0 --output=files tag:deleted | xargs -0 --no-run-if-empty rm -v
+
+echo "Removing inbox tag from sent messages"
+notmuch tag -inbox -- "tag:sent and tag:inbox"
+echo "Removing archived tag from sent messages"
+notmuch tag -archived -- "tag:sent and tag:archived"
 
 # Retrieve new messages
 mbsync --verbose --all
